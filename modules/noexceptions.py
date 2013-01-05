22e1dc4c5fd106ddc3006189b71bae6f06d485c7
begiak <begiak@apertium.vm.bytemark.co.uk>
2013-01-05 23:43:01 +0000

    some fixes and features

--- modules/noexceptions.py
+++ modules/noexceptions.py
@@ -5,6 +5,7 @@ author: mutantmonkey <mutantmonkey@mutantmonkey.in>
 """
 
 def noexceptions(phenny, input):
+   """Tells someone there aren't ever any exceptions"""
    whouser = input.groups()[1]
    if not whouser:
       return phenny.say('NO EXCEPTIONS!')
@@ -13,7 +14,20 @@ def noexceptions(phenny, input):
    phenny.say(response % whouser)
 
 noexceptions.commands = ['noexceptions']
+noexceptions.example = '.noexceptions firespeaker'
 noexceptions.priority = 'low'
+
+def harglebargle(phenny, input):
+   """Tells someone hargle bargle (cf. http://nedroidcomics.livejournal.com/224029.html , http://quotes.firespeaker.org/?id=1415)"""
+   whouser = input.groups()[1]
+   if not whouser:
+      return phenny.say('HARGLE BARGLE!')
+   response = "HARGLE BARGLE, %s!"
+   phenny.say(response % whouser)
+
+harglebargle.commands = ['harglebargle']
+harglebargle.example = '.harglebargle firespeaker'
+harglebargle.priority = 'low'
 
 if __name__ == '__main__':
    print(__doc__.strip())

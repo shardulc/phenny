59e5ec0d8ad7177d891e5c62263ab235702f912f
begiak <begiak@apertium.vm.bytemark.co.uk>
2013-01-06 01:59:19 +0000

    some documentation

--- modules/botfun.py
+++ modules/botfun.py
@@ -9,17 +9,21 @@ import random
 otherbot = "ChanServ"
 
 def botfight(phenny, input):
+    """Make begiak fight other bots in the room."""
     messages = ["hits %s", "punches %s", "kicks %s", "hits %s with a rubber hose", "stabs %s with a clean kitchen knife"]
     response = random.choice(messages)
 
     phenny.do(response % otherbot)
 botfight.commands = ['botfight']
 botfight.priority = 'low'
+botfight.example = '.botfight'
 
 def bothug(phenny, input):
+    """Make begiak hug other bots in the room."""
     phenny.do("hugs %s" % otherbot)
 bothug.commands = ['bothug']
 bothug.priority = 'low'
+bothug.example = '.bothug'
 
 if __name__ == '__main__':
     print(__doc__.strip())

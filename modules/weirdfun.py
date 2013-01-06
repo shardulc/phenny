fab1ae6c13556040d7b9c2b230a9b652ae639daf
Jonathan Washington <jonathan.north.washington@gmail.com>
2013-01-06 01:59:19 +0000

    some documentation

--- modules/weirdfun.py
+++ modules/weirdfun.py
@@ -6,35 +6,44 @@ author: firespeaker <firespeaker@firespeaker.org>
 
 import random
 
-otherbot = "zfe"
+otherweird = "zfe"
 
-def weirdfight(phenny, input):
+def fight(phenny, input):
     """Have begiak fight someone for you."""
-    global otherbot
+    global otherweird
     whouser = input.groups()[1]
     already = False
     if whouser:
-        otherbot = whouser
-        if whouser.lower()=="begiak":
+        otherweird = whouser
+        if whouser.lower()==phenny.nick.lower():
             already = True
             phenny.say("ouch!")
 
     #### "hits %s", "punches %s", "kicks %s",, "stabs %s with a clean kitchen knife",  "hits %s with a rubber hose",
-    messages = [ "hurts himself by accident while trying to attack %s", "directs his Öflazers at %s", "is bored of violence against %s", "thinks you should talk it over with %s first", "cocks %s's beer"]
+    messages = [ "hurts himself by accident while trying to attack %s", "directs his Öflazers at %s", "is bored of violence against %s", "thinks you should talk it over with %s first", "cocks %s's beer", "east %s's hat"]
     response = random.choice(messages)
 
     if not already:
-        phenny.do(response % otherbot)
+        phenny.do(response % otherweird)
 
-weirdfight.commands = ['fight']
-weirdfight.priority = 'low'
-weirdfight.example = '.fight ChanServ'
+fight.commands = ['fight']
+fight.priority = 'low'
+fight.example = '.fight ChanServ'
 
-def weirdhug(phenny, input):
+def hug(phenny, input):
     """Have begiak hug someone for you."""
-    phenny.do("hugs %s" % otherbot)
-weirdhug.commands = ['hug']
-weirdhug.priority = 'low'
-weirdhug.example = '.hug ChanServ'
+    global otherweird
+    whouser = input.groups()[1]
+    if whouser:
+        otherweird = whouser
+
+    if whouser.lower()==phenny.nick.lower():
+        phenny.do("tries but fails.")
+    else:
+        phenny.do("hugs %s" % otherweird)
+
+hug.commands = ['hug']
+hug.priority = 'low'
+hug.example = '.hug ChanServ'
 if __name__ == '__main__':
     print(__doc__.strip())

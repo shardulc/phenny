df75936d740567513e7ba88f8d9d2d01ebd1b9c7
begiak <begiak@apertium.vm.bytemark.co.uk>
2013-01-13 03:44:49 +0000

    further work on github module

--- modules/github.py
+++ modules/github.py
@@ -13,6 +13,41 @@ PORT = 1234
 Handler = None
 httpd = None
 
+def generateReport(data):
+	msgs = []
+	for commit in data['commits']:
+		author = commit['author']['name']
+		comment = commit['message']
+		paths = commit['modified']
+		if comment is None:
+			comment = "Use comments people -_-"
+		else:
+			comment = re.sub("[\n\r]+", " ␍ ", comment.strip())
+		basepath = os.path.commonprefix(paths)
+		textPaths = ""
+		count = 0
+		first = False
+		for path in paths:
+			if count != 0 and first == False:
+				textPaths+=", "
+			count += 1
+			if count < 3:
+				textPaths+=os.path.relpath(path, basepath)
+			elif count >= 3 and first == False:
+				if (len(paths) - 2) > 1:
+					plural = "s"
+				else:
+					plural = ""
+				textPaths+="and %s other file%s" % (str(len(paths) - 2), plural)
+				first = True
+		if len(paths) > 1:
+			finalPath = "%s: %s" % (basepath, textPaths)
+		else:
+			finalPath = paths[0]
+		msg = "%s: %s * r%s: %s: %s\n" % (data['repository']['name'], author, '0000', finalPath, comment.strip())
+		msgs.append(msg)
+	return msgs
+
 class MyHandler(http.server.SimpleHTTPRequestHandler):
 	phenny = None
 
@@ -23,19 +58,20 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
 		self.send_response(403)
 		
 	def do_POST(self):
-		parsed_params = urllib.parse.urlparse(self.path)
-		query_parsed = urllib.parse.parse_qs(parsed_params.query)
+		length = int(self.headers['Content-Length'])
+		post_data = urllib.parse.parse_qs(self.rfile.read(length).decode('utf-8'))
 		
-		try:
-			payload = query_parsed['payload'][0]
-		except KeyError:
+		#try:
+		#	payload = query_parsed['payload'][0]
+		#except KeyError:
 			#self.phenny.say("Something went wrong with getting the data. WHAT.")
 			#self.send_response(403)
 			#return
 		
-		#data = json.loads(payload)
-		self.phenny.say("GOT TEST DATA!!")
-		self.phenny.say(str(query_parsed))
+		data = json.loads(post_data['payload'][0])
+		msgs = generateReport(data)
+		for msg in msgs:
+			self.phenny.say(msg)
 		
 		self.send_response(200)
 		

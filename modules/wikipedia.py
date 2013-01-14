b7a0be85b3a2df399d3b558c7d473a57038534a0
Jonathan Washington <jonathan.north.washington@gmail.com>
2013-01-14 02:24:01 +0000

    Supports mulitple languages

--- modules/wikipedia.py
+++ modules/wikipedia.py
@@ -17,31 +17,28 @@ wikisearch = 'https://%s.wikipedia.org/wiki/Special:Search?' \
 
 langs = ['ar', 'bg', 'ca', 'cs', 'da', 'de', 'en', 'es', 'eo', 'eu', 'fa', 'fr', 'ko', 'hi', 'hr', 'id', 'it', 'he', 'lt', 'hu', 'ms', 'nl', 'ja', 'no', 'pl', 'pt', 'kk', 'ro', 'ru', 'sk', 'sl', 'sr', 'fi', 'sv', 'tr', 'uk', 'vi', 'vo', 'war', 'zh']
 
-def wik(phenny, input): 
-    """Search for something on Wikipedia"""
-    origterm = input.groups()[1].split(' ')
-    
-    lang = "en"
-    if origterm[0] in langs:
-        lang = origterm[0]
-        origterm.pop(0)
-    
-    origterm = ' '.join(origterm)
-    
+def format_term(term):
+    term = urllib.parse.unquote(term)
+    term = urllib.parse.quote(term)
+    term = term[0].upper() + term[1:]
+    term = term.replace(' ', '_')
+    return term
+
+def wikipedia(phenny, origterm, lang):
+    origterm = origterm.strip()
+    lang = lang.strip()
+
     if not origterm: 
         return phenny.say('Perhaps you meant ".wik Zen"?')
 
-    term = urllib.parse.unquote(origterm)
-    term = urllib.parse.quote(origterm)
-    term = term[0].upper() + term[1:]
-    term = term.replace(' ', '_')
+    term = format_term(origterm)
 
     w = wiki.Wiki(wikiapi % lang, wikiuri % lang, wikisearch % lang)
 
     try:
         result = w.search(term)
     except IOError: 
-        error = "Can't connect to en.wikipedia.org ({0})".format(wikiuri.format(term))
+        error = ("Can't connect to %s.wikipedia.org ({0})" % lang).format((wikiuri % lang).format(urllib.parse.unquote(term)))
         return phenny.say(error)
         
         
@@ -50,6 +47,17 @@ def wik(phenny, input):
     else:
         phenny.say('Can\'t find anything in Wikipedia for "{0}".'.format(origterm))
 
+def wik(phenny, input): 
+    """Search for something on Wikipedia"""
+    origterm = input.groups()[1]
+    lang = "en"
+    
+    m = re.match(r'\.wik\.([a-z]{2,3})(?: +(.*))', input.group(0))
+    if m:
+        lang = m.group(1)
+        origterm = m.group(2)
+        
+    wikipedia(phenny, origterm, lang)
 wik.commands = ['wik']
 wik.priority = 'high'
 

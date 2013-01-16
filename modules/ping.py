8011543751966e8fc25043b2efbc54a03c3fb22b
begiak <begiak@apertium.vm.bytemark.co.uk>
2013-01-16 03:53:36 +0000

    some more interesting stuff

--- modules/ping.py
+++ modules/ping.py
@@ -8,10 +8,10 @@ About: http://inamidst.com/phenny/
 import random
 
 def hello(phenny, input): 
-    greeting = random.choice(('Hi', 'Hey', 'Hello'))
+    greeting = random.choice(('Hi', 'Hey', 'Hello', 'What\'s kicking', 'What\'s good', 'Top of the morning', 'Yo', 'What up'))
     punctuation = random.choice(('', '!'))
     phenny.say(greeting + ' ' + input.nick + punctuation)
-hello.rule = r'(?i)(hi|hello|hey) $nickname[ \t]*$'
+hello.rule = r'(?i)(hi|hello|hey|what\'s kickin|how you been|what\'s good|top o\' the mornin|yo) $nickname[ \t]*$'
 
 def interjection(phenny, input): 
     phenny.say(input.nick + '!')

d4ad50a3ebff67046532ffb01acbedaa60f66be6
begiak <begiak@apertium.vm.bytemark.co.uk>
2013-05-19 00:10:25 +0100

    more vanilla ping module

--- modules/ping.py
+++ modules/ping.py
@@ -6,22 +6,78 @@ About: http://inamidst.com/phenny/
 """
 
 import random, re
+import os
+
+# if emptyHellos is True, respond to anyone sending a greeting to no one
+# if emptyHellos is False, don't respond unless addressed
+emptyHellos = False
+
+
+#temporaryMessage = "  If you're a GSoC student, check out http://wiki.apertium.org/wiki/Top_tips_for_GSOC_applications"
+temporaryMessage = None
+
+greetings = ('Hi', 'Hey', 'Hello', 'What\'s kicking', 'What\'s the good word', 'Top of the morning', 'Yo', 'What up', "What's hanging", "In the hood", "Kaixo", "Zer moduz", "Сәлем", "Қалың қалай", "Salom", "Привет", "No bugs is good bugs", "Mitä kuuluu", "حالت چطوره", "როგორა ხარ", "ինչպե՞ս ես", "сайн байна уу", "कैसे हो", "Como vai", "Nasılsın")
+
+def countGuest(phenny, nick):
+    fn = phenny.nick + '-' + phenny.config.host + '.guests.db'
+    global_filename = os.path.join(os.path.expanduser('~/.phenny'), fn)
+    if not os.path.exists(global_filename): 
+        try: f = open(global_filename, 'w')
+        except OSError: pass
+        f.write('')
+        f.close()
+    guests = {}
+    with open(global_filename, 'r') as dbFile:
+        guests = set(dbFile.read().strip().split(','))
+    guests.add(nick)
+    with open(global_filename, 'w') as dbFile:
+        dbFile.write(','.join(guests))
+
+def getGuests(phenny, input):
+    if input.admin:
+
+        fn = phenny.nick + '-' + phenny.config.host + '.guests.db'
+        global_filename = os.path.join(os.path.expanduser('~/.phenny'), fn)
+        if not os.path.exists(global_filename): 
+            try: f = open(global_filename, 'r')
+            except OSError: pass
+            f.close()
+        guests = {}
+        with open(global_filename, 'r') as dbFile:
+            guests = set(dbFile.read().strip().split(','))
+        phenny.say(str(guests))
+    else:
+        phenny.reply("That is an admin-only command.")
+
+
+getGuests.name = 'getGuests'
+getGuests.commands = ['getGuests']
+getGuests.priority = 'low'
+getGuests.thread = False
+
 
 def hello(phenny, input): 
+    global temporaryMessage
+    global emptyHellos
+    global greetings
+
     nickname = phenny.nick
-    temporaryMessage = "  If you're a GSoC student, check out http://wiki.apertium.org/wiki/Top_tips_for_GSOC_applications"
-    greeting = random.choice(('Hi', 'Hey', 'Hello', 'What\'s kicking', 'What\'s the good word', 'Top of the morning', 'Yo', 'What up', "What's hanging", "In the hood", "Kaixo", "Zer moduz", "Сәлем", "Қалың қалай", "Salom", "Привет", "No bugs is good bugs", "Mitä kuuluu", "حالت چطوره", "როგორა ხარ", "ինչպե՞ս ես", "сайн байна уу", "कैसे हो", "Como vai", "Nasılsın"))
-    if not re.search(nickname, input):
+    greeting = random.choice(greetings)
+    if not re.search(nickname, input) and temporaryMessage is not None:
         punctuation = random.choice(('.', '!', ';'))
         phenny.say(greeting + ' ' + input.nick + punctuation +temporaryMessage)
-    else:
+        countGuest(phenny, input.nick)
+    elif emptyHellos:
+        punctuation = random.choice(('', '!'))
+        phenny.say(greeting + ' ' + input.nick + punctuation)
+    elif re.search(nickname, input):
         punctuation = random.choice(('', '!'))
         phenny.say(greeting + ' ' + input.nick + punctuation)
 
 
 #hello.rule = r'(?i)(hi|hello|hey|what\'s kickin[\'g]*|how you been|what\'s (.*)good(.*)|top o[\'f]* the mornin[\'g]*|what\'s hangin[\'g]*|yo|in the hood|in da hood|kaixo|zer moduz|с[әа]л[еао]м|wb) $nickname[ \t]*$'
 #strings = "(?i)(hi|hello|hey|what\'s kickin[\'g]*|how you been|what\'s (.*)good(.*)|top o[\'f]* the mornin[\'g]*|what\'s hangin[\'g]*|yo|in the hood|in da hood|kaixo|zer moduz|с[әа]л[еао]м|s[ae]l[ao]m|wb)"
-hello.rule = r'(?:$nickname[,:]* )?(?i)(hi|hello|hey|what\'s kickin[\'g]*|how you been|what\'s (.*)good(.*)|what\'s up[\.!\?]*|(?:top o[\'f]* the )?mornin[\'g]*|what\'s hangin[\'g]*|yo|in the hood|in da hood|kaixo|zer moduz|с[әа]л[еао]м|s[ae]l[ao]m|wb|mitä kuuluu|როგორა ხარ|ինչպե՞ս ես|сайн байна уу|कैसे हो|como vai|nasılsın(?:ız)?)(?:[,]* $nickname)?[ \t]*$'
+hello.rule = r'(?:$nickname[,:]* )?(?i)(hi|hello|hey|what\'s kickin[\'g]*|how you been|what\'s (.*)good(.*)|what\'s up|(?:top o[\'f]* the )?mornin[\'g]*|what\'s hangin[\'g]*|yo|in the hood|in da hood|kaixo|zer moduz|с[әа]л[еао]м|s[ae]l[ao]m|wb|mitä kuuluu|როგორა ხარ|ինչպե՞ս ես|сайн байна уу|कैसे हो|como vai|nasılsın(?:ız)?)(?:[,]* $nickname)?[ \t!\.\?]*$'
 
 def interjection(phenny, input): 
     phenny.say(input.nick + '!')

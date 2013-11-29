40ed3de4835af184335201e62db996d251d38913
Matthew Ramina <mattramina@gmail.com>
2013-11-28 21:59:46 -0500

    Added language functionality to .wik .wik.<lg> <query> if no language is specified, english is defaulted

--- modules/wikipedia.py
+++ modules/wikipedia.py
@@ -7,27 +7,94 @@ Licensed under the Eiffel Forum License 2.
 http://inamidst.com/phenny/
 """
 
-import re
+import re, urllib.parse, wiki
+from lxml import etree
+import lxml.html
+import lxml.html.clean
 import web
-import wiki
 
-wikiapi = 'https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch={0}&limit=1&prop=snippet&format=json'
-wikiuri = 'https://en.wikipedia.org/wiki/{0}'
-wikisearch = 'https://en.wikipedia.org/wiki/Special:Search?' \
+wikiapi = 'https://%s.wikipedia.org/w/api.php?action=query&list=search&srsearch={0}&limit=1&prop=snippet&format=json'
+wikiuri = 'https://%s.wikipedia.org/wiki/{0}'
+wikisearch = 'https://%s.wikipedia.org/wiki/Special:Search?' \
                           + 'search={0}&fulltext=Search'
 
-def wik(phenny, input): 
-    """.wik <term> - Look up something on Wikipedia."""
+langs = ['ar', 'bg', 'ca', 'cs', 'da', 'de', 'en', 'es', 'eo', 'eu', 'fa', 'fr', 'ko', 'hi', 'hr', 'id', 'it', 'he', 'lt', 'hu', 'ms', 'nl', 'ja', 'no', 'pl', 'pt', 'kk', 'ro', 'ru', 'sk', 'sl', 'sr', 'fi', 'sv', 'tr', 'uk', 'vi', 'vo', 'war', 'zh']
 
-    origterm = input.groups()[1]
-    if not origterm: 
-        return phenny.say('Perhaps you meant ".wik Zen"?')
-
-    term = web.unquote(origterm)
+def format_term(term):
+    term = web.unquote(term)
+    term = web.quote(term)
     term = term[0].upper() + term[1:]
     term = term.replace(' ', '_')
+    return term
 
-    w = wiki.Wiki(wikiapi, wikiuri, wikisearch)
+def format_term_display(term):
+   term = web.unquote(term)
+   term = term[0].upper() + term[1:]
+   term = term.replace(' ', '_')
+   return term
+
+def format_subsection(section):
+   section = section.replace(' ', '_')
+   section = urllib.parse.quote_plus(section)
+   section = section.replace('%', '.')
+   section = section.replace(".3A", ":")
+   return section
+
+def parse_wiki_page(url, term, section = None):
+    try:
+        web_url = web.quote(url).replace("%3A", ":", 1)
+        html = str(web.get(web_url))
+    except:
+        return "A wiki page does not exist for that term."
+    page = lxml.html.fromstring(html)
+    if section is not None:
+        text = page.find(".//span[@id='%s']" % section)
+
+        if text is None:
+            return "That subsection does not exist."
+        text = text.getparent().getnext()
+
+        #a div tag may come before the text
+        while text.tag is not None and text.tag != "p":
+            text = text.getnext()
+        url += "#" + format_term_display(section)
+    else:
+        #Get first 3 paragraphs and find the one most
+        #likely to actually contain text.
+
+        texts = page.findall('.//p')[:4]
+        if len(texts) == 0:
+            return "Unable to find content. Search may be too broad."
+        texts.sort(key=len, reverse=True)
+        text = texts[0]
+
+    sentences = text.text_content().split(". ")   
+    sentence = '"' + sentences[0] + '"'
+
+    maxlength = 440 - len(' - ' + url)
+    if len(sentence.encode('utf-8')) > maxlength: 
+        sentence = sentence[:maxlength]
+        words = sentence[:-5].split(' ')
+        words.pop()
+        sentence = ' '.join(words) + ' [...]'
+
+    return sentence + ' - ' + url
+
+def wikipedia(phenny, origterm, lang):
+    origterm = origterm.strip()
+    lang = lang.strip()
+
+    if not origterm: 
+        return phenny.say('Perhaps you meant ".wik Zen"?')
+    
+    section = None
+
+    if "#" in origterm:
+        origterm, section = origterm.split("#")[:2]
+        section = format_subsection(section)
+    term = format_term(origterm)
+
+    w = wiki.Wiki(wikiapi % lang, wikiuri % lang, wikisearch % lang)
 
     try:
         result = w.search(term)
@@ -36,11 +103,26 @@ def wik(phenny, input):
         return phenny.say(error)
 
     if result is not None: 
-        phenny.say(result)
+        #Disregarding [0], the snippet
+        url = result.split("|")[-1]
+        phenny.say(parse_wiki_page(url, term, section))
+            
     else:
         phenny.say('Can\'t find anything in Wikipedia for "{0}".'.format(origterm))
 
-wik.commands = ['wik']
+def wik(phenny, input): 
+    """Search for something on Wikipedia"""
+    origterm = input.group(3)
+    lang = "en"
+    
+    m = re.match(r'\.(wik|wiki|wikipedia)\.([a-z]{2,3})(?: +(.*))', str(input))
+    if m:
+        lang = m.group(2)
+        origterm = m.group(3)
+
+    wikipedia(phenny, origterm, lang)
+wik.rule = r'\.(wik|wiki|wikipedia)(\.[a-z]{2,3})?\s(?:(.*))'
+#wik.commands = ['wik', 'wiki', 'wikipedia']
 wik.priority = 'high'
 
 if __name__ == '__main__': 

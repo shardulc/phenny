e26e877cdeea9837e9f3210e2602352881dc51e8
Matthew Ramina <mattramina@gmail.com>
2013-11-28 22:11:01 -0500

    Added .whereis sees if a user left an away message with .away

---
+++ modules/away.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+"""
+away.py - Phenny's record or who's away/present module
+"""
+
+import os, re, time, random
+import web
+
+statuses = {"alan": "boom bitchezzzz"}
+
+def whereis(phenny, input):
+	"""Tells you nick's current status."""
+	whereis_nick = input.split(" ")[1]
+	print(input + " --> " + whereis_nick)
+	if (whereis_nick in list(statuses.keys())):
+		phenny.reply(whereis_nick + " said: " + statuses[whereis_nick])
+	else:
+		phenny.reply("Sorry, " + whereis_nick + " seems to be AWOL...")
+whereis.commands = ["whereis"]
+whereis.priority = 'low'
+whereis.example = '.whereis sushain'
+whereis.thread = False
+
+def away(phenny, input):
+	"""Set your status to being away."""
+	nick = input.nick
+	if input.count(" ") == 0:
+		statuses[nick] = "I'm away right now"
+	else:
+		message = str(" ".join(input.split(" ")[1:]))
+		statuses[nick] = message
+away.commands = ['away']
+away.example = '.away eating pie'
+away.priority = 'low'
+away.thread = False
+
+def back(phenny, input):
+	"""Set your status to being available."""
+	nick = input.nick
+	if input.count(" ") == 0:
+		statuses[nick] = "I'm around at the minute"
+	else:
+		message = str(" ".join(input.split(" ")[1:]))
+		statuses[nick] = message
+back.commands = ['back']
+back.example = '.back'
+back.priority = 'low'
+back.thread = False
+
+
+if __name__ == '__main__': 
+    print(__doc__.strip())

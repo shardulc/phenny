6d4cf2a851f12fcdcba696367a4fe797bd4fa6b8
Matthew Ramina <mattramina@gmail.com>
2013-12-01 18:08:02 -0500

    added delete command .queue

--- modules/queue.py
+++ modules/queue.py
@@ -6,7 +6,7 @@ author: mattr555
 import os
 import pickle
 
-commands = '.queue display, .queue new, .queue <name> add, .queue <name> swap, .queue <name> remove'
+commands = '.queue display, .queue new, .queue delete, .queue <name> add, .queue <name> swap, .queue <name> remove'
 
 def filename(phenny):
     name = phenny.nick + '-' + phenny.config.host + '.queue.db'
@@ -69,6 +69,18 @@ def queue(phenny, raw):
                     phenny.reply('Empty queue {} created.'.format(queue_name))
             else:
                 phenny.reply('Syntax: .queue new <name> <item1>, <item2> ...')
+
+        elif command.lower() == 'delete':
+            if raw.group(2):
+                queue_name = raw.group(2)
+                if raw.nick == phenny.queue_data[queue_name]['owner'] or raw.admin:
+                    phenny.queue_data.pop(queue_name)
+                    write_dict(filename(phenny), phenny.queue_data)
+                    phenny.reply('Queue {} deleted.'.format(queue_name))
+                else:
+                    phenny.reply('You aren\'t authorized to do that!')
+            else:
+                phenny.reply('Syntax: .queue delete <name>')
 
         elif command in phenny.queue_data:
             #queue-specific commands

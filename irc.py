e7b4271938efcf64143b8bdf7df08196b78b3a60
Mikhail Ivchenko <ematirov@gmail.com>
2014-12-05 04:18:52 +0400

    Splitting messages is fixed.

--- irc.py
+++ irc.py
@@ -175,27 +175,28 @@ class Bot(asynchat.async_chat):
         max_messages_count = 3
         if len(text) > maxlength:
             for i in range(max_messages_count):
-                if not text:
-                    return
-                if i == max_messages_count - 1:
-                    maxlength = maxlength-3
-                if len(text) > maxlength:
-                    message = text[0:maxlength].decode('utf-8','ignore')
-                    line_break = len(message)
-                    space_found = 0
-                    for j in range(len(message)-1,-1,-1):
-                        if message[j] == " ":
-                            line_break = j
-                            space_found = 1
-                            break
-                    message = text.decode('utf-8','ignore')[0:line_break]
-                    if i == max_messages_count - 1:
-                        message = message + "..."
-                    text=text.decode('utf-8','ignore')[line_break+space_found:].encode('utf-8')
-                else:
-                    message = text.decode('utf-8')
+                # We want to add "..." to last message so we leave place for it
+                if i == max_messages_count-1:
+                    maxlength=maxlength-3
+                message = text[0:maxlength].decode('utf-8','ignore')
+                line_break = len(message)
+                space_found = 0
+                for j in range(len(message)-1,-1,-1):
+                    if message[j] == " ":
+                        line_break = j
+                        space_found = 1
+                        break
+                message = text.decode('utf-8','ignore')[0:line_break]
+                # We want to add "..." to last message
+                if i == max_messages_count-1:
+                    message = message + "..."
                     text = b''
                 self.msg(recipient, message)
+                text=text.decode('utf-8','ignore')[line_break+space_found:].encode('utf-8')
+                if len(text) <= maxlength:
+                    self.msg(recipient,text.decode('utf-8','ignore'))
+                    break
+            self.sending.release()
             return
 
         # No messages within the last 3 seconds? Go ahead!

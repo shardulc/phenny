7365e326fb16c31d261990d4535c48d78b315853
Harold Seefeld <Zaphron@users.noreply.github.com>
2014-12-13 21:44:46 +1100

    save .seen on restart

--- modules/seen.py
+++ modules/seen.py
@@ -3,31 +3,44 @@
 seen.py - Phenny Seen Module
 Copyright 2008, Sean B. Palmer, inamidst.com
 Licensed under the Eiffel Forum License 2.
-
 http://inamidst.com/phenny/
 """
 
 import time, os, shelve, datetime
+import sqlite3
 from tools import deprecated
 
-def f_seen(phenny, input): 
+def f_seen(phenny, input):
     """.seen <nick> - Reports when <nick> was last seen."""
+
+    try:
+        nick = str(input.group(2)).lower()
+    except UnboundLocalError:
+        pass
+
+    if nick == "none":
+        phenny.reply(".seen <nick> - Reports when <nick> was last seen.")
+        return
+
+    fn = phenny.nick + '-' + phenny.config.host + '.logger.db'
+    seen_db = os.path.join(os.path.expanduser('~/.phenny'), fn)
+    seen_conn = sqlite3.connect(seen_db)
     
-    fn = self.nick + '-' + self.config.host + '.logger.db'
-    self.seen_db = os.path.join(os.path.expanduser('~/.phenny'), fn)
-    self.seen_conn = sqlite3.connect(self.seen_db)
-    
-    c = seen.conn.cursor()
-    c.execute(str("select " + input + " from nick"))
-    cLastTime = c.fetchone()[5]
-    cNick = c.fetchone()[4]
-    cChannel = c.fetchone()[0]
+
+    cNick = ""
+    c = seen_conn.cursor()
+    c.execute("SELECT * FROM lines_by_nick WHERE nick = ?", (nick,))
+    try:
+        cLastTime = str(c.fetchone()[5])
+        cNick = str(c.fetchone()[4])
+        cChannel = str(c.fetchone()[0])
+    except TypeError:
+        pass
     c.close()
-    
-    nick = input.group(2).lower()
-    if not hasattr(phenny, 'seen'): 
+
+    if not hasattr(phenny, 'seen'):
         return phenny.msg(input.sender, '?')
-    if nick in phenny.seen: 
+    if nick in phenny.seen:
         channel, t = phenny.seen[nick]
         dt = timesince(datetime.datetime.utcfromtimestamp(t))
         t = time.strftime('%Y-%m-%d %H:%M:%S UTC', time.gmtime(t))
@@ -37,23 +50,23 @@ def f_seen(phenny, input):
     elif nick in cNick:
         dt = timesince(datetime.datetime.utcfromtimestamp(cLastTime))
         t = time.strftime('%Y-%m-%d %H:%M:%S UTC', time.gmtime(cLastTime))
-        
-        msg = "I last saw %s at %s (%s) on %s" % (nick, t, dt, cChannel)
+
+        msg = "I last saw %s at %s (%s) on %s" % (cNick, t, dt, cChannel)
         phenny.reply(msg)
-        
+
     else: phenny.reply("Sorry, I haven't seen %s around." % nick)
 f_seen.name = 'seen'
 f_seen.example = '.seen firespeaker'
 f_seen.rule = (['seen'], r'(\S+)')
 
-@deprecated
-def f_note(self, origin, match, args): 
-    def note(self, origin, match, args): 
-        if not hasattr(self.bot, 'seen'): 
+@deprecated 
+def f_note(self, origin, match, args):
+    def note(self, origin, match, args):
+        if not hasattr(self.bot, 'seen'):
             fn = self.nick + '-' + self.config.host + '.seen'
             path = os.path.join(os.path.expanduser('~/.phenny'), fn)
             self.bot.seen = shelve.open(path)
-        if origin.sender.startswith('#'): 
+        if origin.sender.startswith('#'):
             self.seen[origin.nick.lower()] = (origin.sender, time.time())
             self.seen.sync()
 
@@ -84,5 +97,5 @@ def timesince(td):
 
     return "just now" if len(strings) < 1 else " and ".join(strings) + " ago"
 
-if __name__ == '__main__': 
-    print(__doc__.strip())
+if __name__ == '__main__':
+        print(__doc__.strip())

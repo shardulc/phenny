9903eaf14beef63f4833d0d5893f7178fdca8ba3
sgkryvenko <app1@ukr.net>
2014-12-18 13:23:02 +0200

    Update clock.py

--- modules/clock.py
+++ modules/clock.py
@@ -93,6 +93,27 @@ def scrape_wiki_zones():
                 offset = 0
             offset = int(offset)
         data[code] = offset
+    url = 'http://en.wikipedia.org/wiki/List_of_tz_database_time_zones'
+    resp = web.get(url)
+    h = html.document_fromstring(resp)
+    table = h.find_class('wikitable')[0]
+    for trs in table.findall('tr'):
+        tmr=0
+        for tds in trs.findall('td'):
+            tmr=tmr+1
+            if tmr==3:
+                ctz=tds.find('a').text[tds.find('a').text.find('/')+1:].replace('_',' ')
+                if ctz.find('/')!=-1:
+                    ctz=ctz[ctz.find('/')+1:]
+            if tmr==5:
+                ctu=tds.find('a').text
+                if ctu[ctu.find(':')+1]=='0':
+                    ctu=ctu[:ctu.find(':')]
+                else:
+                    ctu=ctu[:ctu.find(':')]+'.5'
+                if ctu[0]=='−':
+                    ctu='-'+ctu[1:]
+                data[ctz.upper()]=float(ctu)
     return data
 
 def filename(phenny):
@@ -172,7 +193,7 @@ def yi(phenny, input):
     if extraraels == 4: 
         return phenny.say('Yes! PARTAI!')
     elif extraraels == 3:
-    	  return phenny.say('Soon...')
+          return phenny.say('Soon...')
     else: phenny.say('Not yet...')
 yi.commands = ['yi']
 yi.priority = 'low'

b3eea13668f9343143722f5fd031483625c419bf
Harold Seefeld <Zaphron@users.noreply.github.com>
2014-12-21 21:44:57 +1100

    added special add and delete greeting

--- modules/greeting.py
+++ modules/greeting.py
@@ -10,8 +10,16 @@ def setup(self):
     c = self.greeting_conn.cursor()
     c.execute('''create table if not exists lines_by_nick (
         channel     varchar(255),
-        nick        varchar(255)
+        nick        varchar(255),
+        unique (channel, nick) on conflict replace
     );''')
+    c.execute('''create table if not exists special_nicks (
+        message     varchar(255),
+        nick        varchar(255),
+        channel     varchar(255),
+        unique (channel, nick) on conflict replace
+    );''')
+    c.close()
 
 def greeting(phenny, input):
     if not greeting.conn:
@@ -23,8 +31,13 @@ def greeting(phenny, input):
     except UnboundLocalError:
         pass
     
-    if nick.startswith("ap-vbox"):
-        phenny.say(input.nick + ": I see that you have a default nickname enabled. You can change your nickname using: /nick <name>")
+    c = greeting.conn.cursor()
+    c.execute("SELECT * FROM special_nicks WHERE nick = ?", (nick.lower(),))
+    try:
+        phenny.say(input.nick + ": " + str(c.fetchone()[0]))
+    except TypeError:
+        pass
+    c.close()
     
     c = greeting.conn.cursor()
     c.execute("SELECT * FROM lines_by_nick WHERE nick = ?", (nick.lower(),))
@@ -52,6 +65,62 @@ greeting.event = "JOIN"
 greeting.priority = 'low'
 greeting.rule = r'(.*)'
 greeting.thread = False
+
+def greeting_add(phenny, input):
+    if input.group(2) == None:
+        phenny.reply ("You haven't specified a name and message.")
+        return
+    elif len(input.group(2).split(" ")) < 2:
+        phenny.reply ("You haven't specified a message.")
+        return
+    
+    sqlite_data = {
+        'channel': input.sender,
+        'nick': input.group(2).split(" ")[0].lower(),
+        'message': input.group(2).split(" ", 1)[1]
+    }
+    
+    dbconnection = sqlite3.connect(phenny.greeting_db)
+    c = dbconnection.cursor()
+    c.execute('''insert or replace into special_nicks
+                (channel, nick, message)
+                values(
+                    :channel,
+                    :nick,
+                    :message
+                );''', sqlite_data)
+    c.close()
+    
+    c = dbconnection.cursor()
+    c.execute('update special_nicks set message=:message where channel=:channel \
+                and nick=:nick', sqlite_data)
+    c.close()
+    
+    dbconnection.commit()
+    
+    phenny.reply("Successfully added " + input.group(2).split(" ", 1)[0] + " to the special greetings list.")
+    
+greeting_add.rule = (['greeting add'], r'(.*)')
+greeting_add.name = 'greeting add'
+greeting.priority = 'low'
+
+def greeting_del(phenny, input):
+    if input.group(2) == None:
+        phenny.reply ("You haven't specified a name.")
+        return
+    
+    dbconnection = sqlite3.connect(phenny.greeting_db)
+    c = dbconnection.cursor()
+    c.execute("DELETE FROM special_nicks WHERE nick = ? AND channel = ?", (input.group(2).split(" ")[0].lower(), input.sender))
+    c.close()
+    dbconnection.commit()
+    
+    phenny.reply("Successfully deleted " + input.group(2).split(" ", 1)[0] + " from the special greetings list.")
+    
+greeting_del.rule = (['greeting del'], r'(.*)')
+greeting_del.name = 'greeting del'
+greeting.priority = 'low'
+    
 
 if __name__ == '__main__':
     print(__doc__.strip())

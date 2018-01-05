2a778e99f4d1a52d62df5b9587a07078aad75a2d
Grzegorz Stark <gstark0@icloud.com>
2018-01-05 07:48:45 +0100

    Search module replaced with DuckDuckGo Instant Answers (#325)
    Google is deprecated, Bing is removed (since the bot wouldn't even give a reply to Bing commands), and the default search is replaced with DDG's Instant Answers API.

--- modules/search.py
+++ modules/search.py
@@ -9,105 +9,59 @@ http://inamidst.com/phenny/
 
 import re
 import web
+import json
+import requests
+from tools import is_up, truncate
+from modules import more
 
-from googleapiclient.discovery import build
-
-my_api_key = "AIzaSyDiWn9tB9NzxIxkohXXN6GNBNPRep6hIWM"
-my_cse_id = "017800230218291994756:re9_m1koe44"
-
-def google_search(search_term, api_key, cse_id, **kwargs):
-    service = build("customsearch", "v1", developerKey=api_key)
-    res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
-    return res['items']
+ddg_uri = 'https://api.duckduckgo.com/?format=json&pretty=1&q='  
 
 def gsearch(phenny, input):
-    str = input.replace(".g ", "")
-    results = google_search(
-        str, my_api_key, my_cse_id, num=10)
-    for result in results[:1]:
-        phenny.say("{} : {}".format(result['title'], result['link']))
+    phenny.reply('.g is deprecated. Try .search powered by Duck Duck Go instead.')
+gsearch.commands = 'g'
 
-gsearch.commands = ['g']
-gsearch.name = 'g'
-gsearch.example = '.g Apertium'
+def topics(phenny, input):
+    if not is_up('https://api.duckduckgo.com'):
+        return phenny.say('Sorry, DuckDuckGo API is down.')
 
-r_bing = re.compile(r'<h2><a href="([^"]+)"')
-
-def bing_search(query, lang='en-GB'): 
-    query = web.quote(query)
-    base = 'https://www.bing.com/search?mkt=%s&q=' % lang
-    bytes = web.get(base + query)
-    m = r_bing.search(bytes)
-    if m: return m.group(1)
-
-def bing(phenny, input): 
-    """Queries Bing for the specified input."""
+    if not input.group(2): 
+        return phenny.reply('.topics about what?')
     query = input.group(2)
-    if query.startswith(':'): 
-        lang, query = query.split(' ', 1)
-        lang = lang[1:]
-    else: lang = 'en-GB'
-    if not query:
-        return phenny.reply('.bing what?')
 
-    uri = bing_search(query, lang)
-    if uri: 
-        phenny.reply(uri)
-        if not hasattr(phenny.bot, 'last_seen_uri'):
-            phenny.bot.last_seen_uri = {}
-        phenny.bot.last_seen_uri[input.sender] = uri
-    else: phenny.reply("No results found for '%s'." % query)
-bing.commands = ['bing']
-bing.example = '.bing swhack'
+    r = requests.get(ddg_uri + query).json()
+    topics = r['RelatedTopics']
+    if len(topics) == 0:
+        return phenny.say('Sorry, no topics found.')
+    topics_list = []
+    counter = 0
+    for topic in r['RelatedTopics'][:10]:
+        try:
+            topics_list.append(topic['Text'] + ' - ' + topic['FirstURL'])
+        except KeyError:
+            continue
+    more.add_messages(input.nick, phenny, topics_list)
+topics.commands = ['topics']
 
-r_duck = re.compile(r'nofollow" class="[^"]+" href=".*(http.*?)">')
-
-def duck_search(query): 
-    query = query.replace('!', '')
-    query = web.quote(query)
-    uri = 'https://duckduckgo.com/html/?q=%s&kl=uk-en' % query
-    bytes = web.get(uri)
-    m = r_duck.search(bytes)
-    if m: return web.decode(m.group(1))
-
-def duck(phenny, input): 
-    """Queries DuckDuckGo for specified input."""
-    query = input.group(2)
-    if not query: return phenny.reply('.ddg what?')
-
-    uri = duck_search(query)
-    if uri: 
-        phenny.reply(uri)
-        if not hasattr(phenny.bot, 'last_seen_uri'):
-            phenny.bot.last_seen_uri = {}
-        phenny.bot.last_seen_uri[input.sender] = uri
-    else: phenny.reply("No results found for '%s'." % query)
-duck.commands = ['duck', 'ddg']
-duck.example = '.duck football'
-
-def search(phenny, input): 
+def search(phenny, input):
     if not input.group(2): 
         return phenny.reply('.search for what?')
     query = input.group(2)
-    gu = google_search(query) or '-'
-    bu = bing_search(query) or '-'
-    du = duck_search(query) or '-'
 
-    if (gu == bu) and (bu == du): 
-        result = '%s (g, b, d)' % gu
-    elif (gu == bu): 
-        result = '%s (g, b), %s (d)' % (gu, du)
-    elif (bu == du): 
-        result = '%s (b, d), %s (g)' % (bu, gu)
-    elif (gu == du): 
-        result = '%s (g, d), %s (b)' % (gu, bu)
-    else: 
-        if len(gu) > 250: gu = '(extremely long link)'
-        if len(bu) > 150: bu = '(extremely long link)'
-        if len(du) > 150: du = '(extremely long link)'
-        result = '%s (g), %s (b), %s (d)' % (gu, bu, du)
+    if not is_up('https://api.duckduckgo.com'):
+        return phenny.say('Sorry, DuckDuckGo API is down.')
 
-    phenny.reply(result)
+    r = requests.get(ddg_uri + query).json()
+    try:
+        answer = r['AbstractText']
+        answer_url = r['AbstractURL']
+        if answer == '':
+            answer = r['RelatedTopics'][0]['Text']
+            answer_url = r['RelatedTopics'][0]['FirstURL']
+            if answer == '':
+                return phenny.say('Sorry, no result.')
+    except:
+        return phenny.say('Sorry, no result.')
+    phenny.say(truncate(answer, share=' - ' + r['AbstractURL']) + ' - ' + answer_url)
 search.commands = ['search']
 
 def suggest(phenny, input): 
--- modules/test/test_search.py
+++ modules/test/test_search.py
@@ -4,9 +4,8 @@ author: mutantmonkey <mutantmonkey@mutantmonkey.in>
 """
 import re
 import unittest
-from mock import MagicMock
-from modules.search import google_search, gsearch, my_api_key, my_cse_id, \
-        bing_search, bing, duck_search, duck, search, suggest
+from mock import MagicMock, patch
+from modules.search import search, topics, suggest
 from tools import is_up
 from web import unquote
 
@@ -17,52 +16,34 @@ from web import unquote
 # http://stackoverflow.com/a/11206266/1846915
 #
 # update as of 2017-01-14: this has been fixed
-
 class TestSearch(unittest.TestCase):
     def setUp(self):
         self.skip_msg = '{:s} is down, skipping test.'
         self.engines = {
-            'Bing': 'https://bing.com',
-            'DuckDuckGo': 'https://duckduckgo.com',
+            'DuckDuckGo': 'https://api.duckduckgo.com',
             'Suggestion script': 'http://websitedev.de/temp-bin/'
         }
         self.phenny = MagicMock()
         self.input = MagicMock()
 
-    def test_bing_search(self):
-        if not is_up(self.engines['Bing']):
-            self.skipTest(self.skip_msg.format('Bing'))
-        out = bing_search('phenny')
-        m = re.match('^https?://.*$', out, flags=re.UNICODE)
-        self.assertTrue(m)
-
-    def test_bing(self):
-        if not is_up(self.engines['Bing']):
-            self.skipTest(self.skip_msg.format('Bing'))
-        self.input.group.return_value = 'swhack'
-        bing(self.phenny, self.input)
-        self.assertTrue(self.phenny.reply.called)
-
-    def test_duck_search(self):
-        if not is_up(self.engines['DuckDuckGo']):
-            self.skipTest(self.skip_msg.format('DuckDuckGo'))
-        out = unquote(duck_search('phenny'))
-        m = re.match(r'^https?://.*$', out, flags=re.UNICODE)
-        self.assertTrue(m)
-
-    def test_duck(self):
-        if not is_up(self.engines['DuckDuckGo']):
-            self.skipTest(self.skip_msg.format('DuckDuckGo'))
-        self.input.group.return_value = 'swhack'
-        duck(self.phenny, self.input)
-        self.assertTrue(self.phenny.reply.called)
+    @patch('modules.search.requests.get')
+    def test_requests(self, mock_get):
+        mock_response = MagicMock()
+        mock_response.json.return_value = {
+            "AbstractText" : "TestText",
+            "AbstractURL" : "https://testurl.com"
+        }
+        mock_get.return_value = mock_response
+        self.input.group.return_value = 'test'
+        search(self.phenny, self.input)
+        self.phenny.say.assert_called_with('TestText - https://testurl.com')
 
     def test_search(self):
-        if not (self.engines['DuckDuckGo'] or self.engines['Bing'] or self.engines['Google']):
-            self.skipTest('All search engines are down, skipping test.')
-        self.input.group.return_value = 'vtluug'
-        duck(self.phenny, self.input)
-        self.assertTrue(self.phenny.reply.called)
+        if not is_up(self.engines['DuckDuckGo']):
+            self.skipTest(self.skip_msg.format('DuckDuckGo'))
+        self.input.group.return_value = 'Apertium'
+        search(self.phenny, self.input)
+        self.assertTrue(self.phenny.say.called)
 
     def test_suggest(self):
         if not is_up(self.engines['Suggestion script']):

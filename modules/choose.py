b4520da2b58a1e2bffe084bdcfa0d90de8cb8233
Robin Richtsfeld <robin.richtsfeld@gmail.com>
2018-03-10 01:51:41 +0100

    Clean choose.py

--- modules/choose.py
+++ modules/choose.py
@@ -4,18 +4,15 @@
 choose.py - sometimes you just can't decide, a phenny module
 """
 
-import re, random
+import random
+import re
 
 def choose(phenny, input):
     """.choose <red> <blue> - for when you just can't decide"""
     origterm = input.groups()[1]
     if not origterm:
         return phenny.say(".choose <red> <blue> - for when you just can't decide")
-    c = re.findall(r'([^,]+)', origterm)
-    if len(c) == 1:
-        c = re.findall(r'(\S+)', origterm)
-        if len(c) == 1:
-            return phenny.reply("%s" % (c[0].strip()))
+    c = re.findall(r'([^, ]+)', origterm)
     fate = random.choice(c).strip()
     return phenny.reply("%s" % (fate))
 choose.rule = (['choose'], r'(.*)')
